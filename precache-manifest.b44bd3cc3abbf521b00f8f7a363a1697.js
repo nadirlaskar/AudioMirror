44a461e9c582707f1c313a50924bac559e2e5648
nadirlaskar <nadirlaskar@gmail.com>
2020-08-12 02:43:34 +0530

    Updates

---
+++ precache-manifest.b44bd3cc3abbf521b00f8f7a363a1697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd65118da0c7d1f8be5aea98dc640e06",
+    "url": "/AudioMirror/index.html"
+  },
+  {
+    "revision": "e1f908d2b12225d2694f",
+    "url": "/AudioMirror/static/css/main.37ead0c3.chunk.css"
+  },
+  {
+    "revision": "2195c6759e28d7670bb1",
+    "url": "/AudioMirror/static/js/2.264bd936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AudioMirror/static/js/2.264bd936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f908d2b12225d2694f",
+    "url": "/AudioMirror/static/js/main.c77ff17b.chunk.js"
+  },
+  {
+    "revision": "ab478c476deccbe827cd",
+    "url": "/AudioMirror/static/js/runtime-main.8abe4741.js"
+  },
+  {
+    "revision": "45e516f87a246c7e048adc28d0c2d3b5",
+    "url": "/AudioMirror/static/media/logo.45e516f8.svg"
+  }
+]);
